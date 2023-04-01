4a1b46063226e44de3ff47f465a6b9987daaff00
khanhpnp <79681871+khanh-pnp@users.noreply.github.com>
2023-04-01 14:31:28 +0700

    Update requirement.txt

--- samples/config.py
+++ samples/config.py
@@ -1,7 +1,7 @@
 # Set to your Adafruit IO key.
 
 # Remember, your key is a secret so make sure not to publish it when you publish this code!
-ADAFRUIT_IO_KEY = ''
+ADAFRUIT_IO_KEY = 'aio_CGTk54tUUZctkMG1BbkNyNuZpgLc'
 
 # Set to your Adafruit IO username. (go to https://accounts.adafruit.com to find your username)
 ADAFRUIT_IO_USERNAME = 'khanhpnp'
@@ -10,5 +10,5 @@ ADAFRUIT_IO_USERNAME = 'khanhpnp'
 IO_FEED_USERNAME = 'khanhpnp'
 
 # Make sure you have read AND write access to this feed to publish.
-IO_FEED = 'SHARED_AIO_FEED_NAME'
+IO_FEED = 'logs'
 
